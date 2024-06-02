d0038a44fca346ce78ede068c0d2cda1a6fa1af1
ERFaN <erfanshirin120@gmail.com>
2024-06-02 19:20:17 +0330

    copy button function configured for mobiles

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,17 +1,20 @@
+import connectDB from "@/utils/connectDB";
+import EstateProfile from "@/models/EstateProfile";
 import BuyResidentialPage from "@/template/BuyResidentialPage";
 
 const BuyResidential = async ({ searchParams }) => {
-  const res = await fetch(`${process.env.FETCH_URL}/api/profile`, {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  let finalData = data.data;
+  await connectDB();
+  const profiles = await EstateProfile.find({ published: true }).select(
+    "-userId "
+  );
+
+  let finalData = profiles;
   if (searchParams.category) {
     finalData = finalData.filter(
       (item) => item.category === searchParams.category
     );
   }
-  if (data.error) return <h3>مشکلی پیش آمده است!</h3>;
+  if (!profiles) return <h3>مشکلی پیش آمده است!</h3>;
 
   return <BuyResidentialPage data={finalData} />;
 };
--- src/components/module/CopyButton.js
+++ src/components/module/CopyButton.js
@@ -4,21 +4,15 @@ import { MdContentCopy } from "react-icons/md";
 import { LiaCheckDoubleSolid } from "react-icons/lia";
 import toast from "react-hot-toast";
 import styles from "@/module/CopyButton.module.css";
+import CopyToClipboard from "react-copy-to-clipboard";
 const CopyButton = ({ phone }) => {
   const [copy, setCopy] = useState(false);
   const copyHandler = () => {
-    navigator.clipboard
-      .writeText(phone)
-      .then(() => {
-        toast.success("شماره تماس کپی شد");
-        setCopy(true);
-        setTimeout(() => {
-          setCopy(false);
-        }, 5000); // Reset copied state after 3 seconds
-      })
-      .catch((err) => {
-        toast.error("مشکلی پیش آمده!");
-      });
+    toast.success("شماره تماس کپی شد");
+    setCopy(true);
+    setTimeout(() => {
+      setCopy(false);
+    }, 5000); // Reset copied state after 5 seconds
   };
   return (
     <>
@@ -27,12 +21,29 @@ const CopyButton = ({ phone }) => {
           <LiaCheckDoubleSolid />
         </span>
       ) : (
-        <span id={styles["copyBtn"]} onClick={copyHandler}>
-          <MdContentCopy />
-        </span>
+        <CopyToClipboard text={phone}>
+          <span id={styles["copyBtn"]} onClick={copyHandler}>
+            <MdContentCopy />
+          </span>
+        </CopyToClipboard>
       )}
     </>
   );
 };
 
 export default CopyButton;
+
+// const copyHandler = () => {
+//   navigator.clipboard
+//     .writeText(phone)
+//     .then(() => {
+//       toast.success("شماره تماس کپی شد");
+//       setCopy(true);
+//       setTimeout(() => {
+//         setCopy(false);
+//       }, 5000); // Reset copied state after 3 seconds
+//     })
+//     .catch((err) => {
+//       toast.error("مشکلی پیش آمده!");
+//     });
+// };
